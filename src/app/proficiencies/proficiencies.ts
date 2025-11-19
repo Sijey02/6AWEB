a36bd0ae519b30a7a512fbf10b3babd872fae502
Sijey02 <cjmiranda0402@gmail.com>
2025-11-19 15:56:18 +0800

    commit

---
+++ src/app/proficiencies/proficiencies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proficiencies',
+  imports: [],
+  templateUrl: './proficiencies.html',
+  styleUrl: './proficiencies.css',
+})
+export class Proficiencies {
+
+}
